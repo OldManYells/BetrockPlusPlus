0afab4e63cbfef031a017e94258be69199e8bd52
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-04-14 12:39:36 -0400

    Added world boilerplate

---
+++ src/bpp_shared/world/ClientPos.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026, Aidan <JcbbcEnjoyer>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+*/
+#pragma once
+#include <numeric_structs.h>
+
+// This struct stores the client's position as well as some helper functions to get their current chunk and exact block coords. 
+// This is used by the world manager to determine which chunks to load and unload around a player
+struct ClientPosition {
+	Vec3 pos{ 0.0, 0.0, 0.0 };
+
+	inline Int2 getChunkPos() const {
+		return Int2{
+			static_cast<int>(std::floor(pos.x)) >> 4,
+			static_cast<int>(std::floor(pos.z)) >> 4
+		};
+	}
+
+	inline Int3 getBlockPos() const {
+		return Int3{
+			static_cast<int>(std::floor(pos.x)),
+			static_cast<int>(std::floor(pos.y)),
+			static_cast<int>(std::floor(pos.z))
+		};
+	}
+
+	inline Int2 getRegionPos() const {
+		return Int2{
+			static_cast<int>(std::floor(pos.x)) >> 9,
+			static_cast<int>(std::floor(pos.z)) >> 9
+		};
+	}
+};
---
+++ src/bpp_shared/world/World.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026, Aidan <JcbbcEnjoyer>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+*/
+
+// The world manager acts like a wrapper around the chunk manager and lighting manager.
+// It handles all world-related operations and provides a simple interface for the rest of the code to interact with the world.
+struct WorldManager {
+
+};
