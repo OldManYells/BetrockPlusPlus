8dda6feccec87965461b1c55c3af27dca9fbe214
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-05-12 16:04:09 -0400

    Update entity.h

--- src/bpp_shared/entities/entity.h
+++ src/bpp_shared/entities/entity.h
@@ -12,17 +12,59 @@ struct entity {
 	boolean preventEntitySpawning = false;
 	entity* riddenByEntity;
 	entity* ridingEntity;
+
+	// Position last tick
 	double prevPosX;
 	double prevPosY;
 	double prevPosZ;
+
+	// Position this tick
 	double posX;
 	double posY;
 	double posZ;
+
+	// Velocity
 	double motionX;
 	double motionY;
 	double motionZ;
+
+	// Look direction
 	float rotationYaw;
 	float rotationPitch;
+
+	// Look direction last tick
 	float prevRotationYaw;
 	float prevRotationPitch;
-};
+
+	// Collider
+	AABB boundingBox{ 0, 0, 0, 0, 0, 0 };
+
+	bool onGround = false;
+	bool collidedHorizontally = false;
+	bool collidedVertically = false;
+	bool collided = false;
+	bool beenAttacked = false;
+	bool inWeb = false;
+	bool hasPhysics = true;
+	bool isDead = false;
+
+	// Vertical offset from posY to the bottom of the bounding box (feet position)
+	float yOffset = 0.0F;
+
+	// Collision box dimensions in blocks
+	float width = 0.6F;
+	float height = 1.8F;
+
+	// Distance walked last tick
+	float prevDistanceWalked = 0.0f;
+	
+	// Distance walked this tick
+	float distanceWalked = 0.0f;
+
+	float fallDistance = 0.0f;
+
+	// Position the tick before last
+	double prevPrevPosX;
+	double prevPrevPosY;
+	double prevPrevPosZ;
+ };
